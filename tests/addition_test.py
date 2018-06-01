c1d83b808c08a47c6b5af85e262607c9c4d2e505
iztok <kardenal.mendoza@gmail.com>
2018-06-01 10:14:17 +0100

    looking good in terms of basic structrue... - class based - using latex math environment for all expressions..

--- tests/addition_test.py
+++ tests/addition_test.py
@@ -1,4 +1,6 @@
 from mathsmonkey.addition import addition
 
-a = addition()
-a.gen(100,2,0,4, 3)
+a = addition('.', 'simple_addition')
+a.gen(5, 2, 1, 100, 3)
+a.gen(4, 2, 0, 100, 3)
+a.gen(3, 2, 1, 100, 4)
