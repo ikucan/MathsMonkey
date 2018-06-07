78608a50e477026b4fb4cc2f9d01ab08e1310013
iztok <kardenal.mendoza@gmail.com>
2018-06-07 08:54:06 +0100

    ..

---
+++ tests/smpl/mixd_test.py
@@ -0,0 +1,19 @@
+from mathsmonkey.smpl.smpl_mxd_add_sub import mix_add_sub
+
+a = mix_add_sub('../../pdfs/smpl/', 'simple_addition_subtraction')
+
+if False:
+    for n_nums in range(2, 6):
+        for n_dgts in range (1, 4):
+            for dgt_var in range (0, n_dgts):
+                n_cols = 2
+                if n_nums * n_dgts < 15:
+                    n_cols += 1
+                if n_nums * n_dgts < 10:
+                    n_cols += 1
+                if n_nums * n_dgts < 5:
+                    n_cols += 1
+                a.gen(n_nums, n_dgts, dgt_var, 100, n_cols)
+
+a.gen(5, 3, 2, 100, 2)
+
